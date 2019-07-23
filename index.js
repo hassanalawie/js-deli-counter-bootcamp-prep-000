6eef6b7b6d0981781a9b17a3de116242d0fc0de6
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-23 20:03:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 
-function takeANumber(line,name){
+function takeANumber(name){
   katzDeliLine.push(name)
   return "Welcome, "+name+". You are number "+ katzDeliLine.length + " in line"
 }
