d05b16c6c74680591a646f62701deb2190fd64fc
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-23 19:57:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var katzDeliLine = [];
+
+function takeANumber(line, name){
+  return "Welcome ${name}. You are number ${line} in line"
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length>0)
+  {
+    var temp = katzDeliLine[0]
+    katzDeliLine.slice(1)
+  }
+  else
+  {
+    temp="There is nobody waiting to be served!"
+  }
+return temp
+}
+
+function currentLine(katzDeliLine){
+var array=[]
+if (katzDeliLine.length > 0){
+  for (var i=1; i<katzDeliLine.length; i++){
+    array.push(i+ ". " + katzDeliLine[i-1])
+  }}
+else{
+  array= "The line is currently empty"
+}
+  return array
+}
